79b05fc8454a92123ddfcefcbee8a57a2650a7fb
kiyokyle <30451559+kiyokyle@users.noreply.github.com>
2018-05-15 07:29:35 +1000

    Added Refrigerated Truck Test
    - Test for refrigerated truck have been implemented

---
+++ src/Tests/Refrigerated_Truck_Test.java
@@ -0,0 +1,65 @@
+package Tests;
+
+import org.junit.*;
+
+public class Refrigerated_Truck_Test {
+	
+	private RefrigeratedTruck testTruck;
+	private Stock testStock = new Stock();
+	RefrigeratedItem beef = new RefrigeratedItem("beef",12.00,	17.00,	425.0,	550.0,	3.0);
+
+	/**
+	 * Sets up before the tests
+	 */
+	@Before
+	public void setUp() {
+		testTruck = null;
+		testStock.add(beans);
+		testStock.add(beef);
+	}
+	/**
+	 * Tests is the truck is refrigerated truck
+	 */
+	@Test
+	public void testClass() {
+		testTruck = new refrigeratedTruck(testStock);
+		assertEquals(RefrigeratedTruck.class,testTruck.getClass());
+	}
+	/**
+	 * Gets the temperature of the truck, Equall to the temperature of the lowest item in the stock
+	 */
+	@Test
+	public void testSetTemp() {
+		testTruck = new refrigeratedTruck(testStock);
+		assertEquals(3.0, testTruck.getTemperature());
+	}
+	
+	/*
+	 * Checks to see if the cargoLimit is at 800;
+	 */
+	@Test
+	public void TestCargoLimit() {
+		testTruck = new RefrigeratedTruck(testStock);
+		assertEquals(800, testTruck.getCargoLimit());		
+	}
+	
+	/**
+	 * Gets the cargosize of the items currently inside the truck
+	 */
+	@Test
+	public void testCargoSize() {
+		testTruck = new RefrigeratedTruck(testStock);
+		assertEquals(425.0, testTruck.getCargoSize());
+	}
+
+	/**
+	 * Tests that the calculation for the price of the truck is correct
+	 */
+	@Test
+	public void testCargoCost() {
+		testTruck = new RefrigeratedTruck(testStock);
+		assertEquals(462, testTruck.getTruckCost());
+	}
+	
+
+}
