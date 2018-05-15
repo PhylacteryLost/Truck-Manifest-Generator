a36c24a2cafd10848fa57076bf70f9ca15a1901c
Clinton Hodge <36693901+Buldron@users.noreply.github.com>
2018-05-15 16:19:52 +1000

    Stock Test

--- src/Tests/Stock_Test.java
+++ src/Tests/Stock_Test.java
@@ -6,37 +6,77 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class Stock_Test 
 {
-	Stock testStoreInventory;
-	List<Item> items = new ArrayList<Item>();
-	Item item_1, item_2, item_3;
+	Stock inventory = new Stock();
+	
+	Item item_1, item_2;
+	RefrigeratedItem item_3;
 	
 	@Before
 	public void TestAddItemsToInventory()
 	{		
-		// Create test items and add to a ArrayList.
-		item_1 = new Item("test_item_1", 35.00, 40.00, 20, 30, 54.67);
-		item_2 = new Item("test_item_1", 35.00, 40.00, 20, 30, 54.67);
-		item_3 = new Item("test_item_3", 35.00, 40.00, 20, 30, 54.67);
-		items.add(item_1);
-		items.add(item_2);
-		items.add(item_3);
+		// Create test items.
+		item_1 = new Item("warm rock", 1, 3, 100, 170);
+		item_2 = new Item("warm matt", 3, 1, 1300, 17000);
+		item_3 = new RefrigeratedItem("cold item", 3, 1, 1300, 17000, 12.0);
 		
-		// Add all items to stock.
-		testStoreInventory.AddToInventory(items);			
+		// Add items.
+		inventory.addItem(item_1);
+		inventory.addItem(item_2);
+		inventory.addItem(item_3);		
 	}
 	
-	@Test	// Test if items array list same as one passed in.
-	public void TestGetInventoryList() throws Exception
+	@Test
+	public void TestAddItems()
 	{
-		assertEquals(items, testStoreInventory.GetInventoryList());
+		Stock tStock = new Stock();
+		for(int i = 0; i < 500; i++)
+		{
+			Item item = new Item(("item"+i), 1, 3, 100, 170);
+			tStock.addItem(item);
+		}
+		
+		assertEquals(true, true);
 	}
 	
-	@Test	// Test for number of items in inventory.
+	@Test	
+	public void TestGetInventoryList() throws Exception
+	{		
+		// Create test stock.
+		Stock stock = new Stock();
+		stock.addItem(item_1);
+		stock.addItem(item_2);
+		stock.addItem(item_3);
+		
+		// Compare test stock to retrieved stock.
+		assertEquals(inventory.getStock(), stock.getStock());
+	}
+	
+	@Test	
 	public void TestGetItemQuantity()
 	{
-		assertEquals(3, testStoreInventory.GetItemQuantity());
+		// Test for number of items in inventory.
+		assertEquals(100, (int)item_1.getQuantity());
+	}
+	
+	@Test
+	public void TestGetLength()
+	{
+		Stock stock = new Stock();
+		for(int i = 0; i < 200; i++)
+		{
+			Item item = new Item(("item"+i), 1, 3, 100, 170);
+			stock.addItem(item);
+		}
+		
+		assertEquals(200, stock.getLength());
+	}
+	
+	@Test
+	public void TestGetItem()
+	{
+		// Test if get item works.
+		assertEquals(item_1, inventory.getItem(0));
 	}
 }
