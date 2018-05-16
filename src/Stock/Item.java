bd3c2ec1c9f5c1604737c7cd2d12eda754a80e03
Klarinaaa <30451559+Klarinaaa@users.noreply.github.com>
2018-05-16 23:57:51 +1000

    Added javadoc comments to Item, RefrigereatedItem and stock
    - Added javadoc comments to all methods and constructors of each class

--- src/Stock/Item.java
+++ src/Stock/Item.java
@@ -9,10 +9,18 @@ public class Item {
 	private int reorderAmount;
 	private int quantity;
 
+
 	/*
-	 * Ceates a food item using a string as the name, 2 doubles for the selling and manufacturing cost
-	 * and two integers for the reorder point and reorder amount
+	 * Creates an item using a string for the name, doubles to represent a selling price and manufacturing price, 
+	 * And integers to represent reorderpoints and reorderAmounts 
 	 * 
+	 * 
+	 * @Author Kyle Langton
+	 * @param Doubles: sellprice , selling price of the item
+	 * @param double manfprice, price required to manufacture the item
+	 * @param integer reorderPoint, the re-order point for the item
+	 * @param integer reorderamount, the price of the reorder
+	 * @return An Item with the above properties
 	 */
 	public Item(String name, double sellPrice, double manfPrice, int reorderPoint, int reorderAmount) {
 		itemName = name;
@@ -25,33 +33,57 @@ public class Item {
 	
 	/*
 	 * Simple method which returns the name of the item
-	 * RETURN: Name of the item as a string
+	 *
+	 * 
+	 * @Author Kyle Langton
+	 * @return the item name as a string
 	 */
 	public Object GetName() {
 		// TODO Auto-generated method stub
 		return itemName;
 	}
 	
+	/* gets the manufacture price of the item
+	 * @Author Kyle Langton
+	 * @return the manufacture cost of the item as a double
+	 * 
+	 */
 	public double GetManufacturePrice() {
 		// TODO Auto-generated method stub
 		return manfCost;
 	}
 	
+	/* gets the selling price of the item
+	 * @Author Kyle Langton
+	 * @return the selling price of the item as a double
+	 */
 	public double GetSellPrice() {
 		// TODO Auto-generated method stub
 		return sellingPrice;
 	}
 	
+	/* gets the reorderpoint of the item
+	 * @Author Kyle Langton
+	 * @return returns the reorderpoint of the item as a integer
+	 */
 	public int GetReorderPoint() {
 		// TODO Auto-generated method stub
 		return restockPoint;
 	}
 	
+	/* gets the re-order amount of the item
+	 * @Author Kyle Langton
+	 * @return returns the re-order amount of the item as an integer
+	 */
 	public int GetReorderAmount() {
 		// TODO Auto-generated method stub
 		return reorderAmount;
 	}
 	
+	/* gets the quantity of the items
+	 * @Author Kyle Langton
+	 * @return returns the item quantity as an integer
+	 */
 	public int getQuantity() {
 		// TODO Auto-generated method stub
 		return quantity;
--- src/Stock/Stock.java
+++ src/Stock/Stock.java
@@ -6,29 +6,56 @@ public class Stock {
 	
 	ArrayList<Item> inventory;
 	
+	/*
+	 *  Creates an array list to be called
+	 *  @author Kyle Langton
+	 */
 	public Stock(){
 		inventory = new ArrayList<Item>();
 	}
 	
-
+	/*
+	 *  adds the given item parameter to the array list
+	 *  
+	 *  
+	 * @param an Item to be added the the arraylist.
+	 * @author Kyle Langton
+	 * 
+	 */
 	public void addItem(Item item) {
 		// TODO Auto-generated method stub
 		inventory.add(item);
 	}
 
-
+	/*
+	 * gets the arraylist
+	 * 
+	 * @return returns the arraylist 
+	 * @author Kyle Langton
+	 */
 	public ArrayList<Item> getStock() {
 		// TODO Auto-generated method stub
 		return inventory;
 	}
 
-
+	/*
+	 *  gets the length of the arraylist
+	 *  
+	 * @return returns the size of the arraylist as an integer
+	 * @author Kyle Langton
+	 */
 	public int getLength() {
 		// TODO Auto-generated method stub
 		return inventory.size();
 	}
 
-
+	/* gets the item given at given number
+	 * 
+	 * 
+	 * @param itemNumber, a given index number as an integer
+	 * @return returns the item given at the location of the paramaters integer
+	 * @author Kyle Langton
+	 */
 	public Object getItem(int itemNumber) {
 		// TODO Auto-generated method stub
 		return inventory.get(itemNumber);
