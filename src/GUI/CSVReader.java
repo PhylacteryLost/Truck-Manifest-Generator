b519d7b1742afac2a25ed6643fb0a296b4669a8e
Klarinaaa <30451559+Klarinaaa@users.noreply.github.com>
2018-05-20 02:10:58 +1000

    Added javadoc comments

--- src/GUI/CSVReader.java
+++ src/GUI/CSVReader.java
@@ -8,6 +8,14 @@ import java.util.ArrayList;
 
 public class CSVReader {
 	
+	/*
+	 *  Reads the CSV file by line breaking the strings up by ,'s
+	 *  Each string is places into an array for easy gathering.
+	 *  
+	 *  @author Kyle Langton
+	 *  @param File: The CSV file
+	 *  @return ArrayList<String[]> Returns an arrayList of string arrays containing the seperated values of each line
+	 */
 	public static ArrayList<String[]> readCSV(File fileName) throws IOException {
 		FileReader reader = new FileReader(fileName);
 		BufferedReader buffReader = new BufferedReader(reader);
