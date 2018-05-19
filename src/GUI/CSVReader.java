5006cfd7973462dede77be82d96a3478f1735f9d
Klarinaaa <30451559+Klarinaaa@users.noreply.github.com>
2018-05-20 02:07:51 +1000

    Added CSV Reader
    - Still Need to do Javadoc comments

--- src/GUI/CSVReader.java
+++ src/GUI/CSVReader.java
@@ -1,5 +1,24 @@
 package GUI;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class CSVReader {
+	
+	public static ArrayList<String[]> readCSV(File fileName) throws IOException {
+		FileReader reader = new FileReader(fileName);
+		BufferedReader buffReader = new BufferedReader(reader);
+		ArrayList<String[]> stringsBrocken = new ArrayList<String[]>();
+		String textDocument;
+		while ((textDocument = buffReader.readLine()) != null) {
+				String[] splitStrings = textDocument.split(",");
+				stringsBrocken.add(splitStrings);
+		}
+		buffReader.close();
+		return stringsBrocken;
+}
 
 }
