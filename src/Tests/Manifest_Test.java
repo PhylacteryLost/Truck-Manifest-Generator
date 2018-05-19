9266e13ee93b2fd32d4a474d06babd3b99f4be32
Klarinaaa <30451559+Klarinaaa@users.noreply.github.com>
2018-05-19 16:29:46 +1000

    Manifest Test Added
    Added the Manifest Tests, The 2nd last test should fix itself after the classes have been added

---
+++ src/Tests/Manifest_Test.java
@@ -0,0 +1,57 @@
+package Tests;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+
+public class Manifest_Test {
+	
+	Stock testStock = new Stock();
+	
+	Manifest testManifest;
+	OrdinaryTruck testOrd = new OrdinaryTruck(testStock);
+	RefrigeratedTruck testRef = new RefrigeratedTruck(testStock);
+	
+	Arraylist<Truck> testColl; 
+	
+	
+	@Before
+	public void setUp() {
+		testManifest = null;
+	}
+	
+	
+	@Test
+	public void testClass() {
+		testManifest = new Manifest();
+		assertEquals(Manifest.class, testManifest.getClass());
+	}
+	
+	@Test
+	public void testType() {
+		testManifest = new Manifest();
+		assertEquals(testColl.class , testManifest.getManifest());
+		
+	}
+	
+	
+	@Test
+	public void testAdd() {
+		testManifest = new Manifest();
+		testManifest.addTruck(testOrd);
+		assertEquals(testOrd, testManifest.getTruck(0));
+	}
+	
+	@Test
+	public void testLength() {
+		testManifest = new Manifest();
+		testManifest.addTruck(testOrd);
+		testManifest.addTruck(test);
+		assertEquals(2, testManifest.getLength());	
+	}
+	
+	
+
+}
