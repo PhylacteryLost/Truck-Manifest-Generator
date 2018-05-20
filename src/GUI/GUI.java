26a2417bc920d6ede0ea1d7949d7373ca6b65be6
Klarinaaa <30451559+Klarinaaa@users.noreply.github.com>
2018-05-21 00:28:25 +1000

    Added Import Item Properties
    - Item Properties Has been added and curently works.
    - One issue with the program not exiting properly after you close the exit dialog

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -2,11 +2,19 @@ package GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.*;
 
+import Assignment.Program.Item;
+import Stock.Stock;
 import Stock.Store;
+import Stock.Item;
 
 public class GUI {
 	
@@ -19,6 +27,7 @@ public class GUI {
 		// Store variables 
 		
 		Store supermarket = Store.GetStore();
+		Stock storeInventory = new Stock();
 		double storeCapital = supermarket.GetCapital();
 		
 		
@@ -61,6 +70,95 @@ public class GUI {
 		exportManifest.setEnabled(false);
 		
 		// Action Listeners
+		
+		importItemProperties.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame csvChooserFrame = new JFrame();
+				JFileChooser csvChooser = new JFileChooser();
+				csvChooser.setFileFilter(new FileNameExtensionFilter(".csv", "csv"));
+				csvChooserFrame.add(csvChooser);
+				int status = csvChooser.showOpenDialog(null);
+				
+				if (status == JFileChooser.CANCEL_OPTION) {
+					csvChooserFrame.dispose();
+				}
+				if (status == JFileChooser.OPEN_DIALOG) {
+					String fileName = csvChooser.getSelectedFile().getName();					
+					if (fileName.matches(".*.csv")) {
+						ArrayList<String[]> readValues = new ArrayList<String[]>();
+						
+						try {
+							readValues = CSVReader.readCSV(csvChooser.getSelectedFile());
+							//manifestFilePath = csvChooser.getSelectedFile().getParentFile().getPath()+"\\";
+							
+							String regex = "item_properties";
+							//manifestFilePath.replaceAll("item", "manifest");
+							//System.out.println(manifestFilePath);
+							System.out.println(regex);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+						for (int stringcount = 0; stringcount < readValues.size(); stringcount++) {
+							if (readValues.get(stringcount).length == 5) {
+								
+							Item foodItem = new Item(readValues.get(stringcount)[0],Double.parseDouble(readValues.get(stringcount)[1]),Double.parseDouble(readValues.get(stringcount)[2])
+									,Integer.parseInt(readValues.get(stringcount)[3]),Integer.parseInt(readValues.get(stringcount)[4]));
+							
+							
+							
+							Object[] tempData = { foodItem.GetName(), foodItem.getQuantity(), foodItem.GetManufacturePrice(), 
+									foodItem.GetSellPrice(), foodItem.GetReorderAmount(), foodItem.GetReorderPoint(), null };
+							tableSettings.addRow(tempData);
+							
+							// ---- TEMP STORE STUFF ----
+							storeInventory.addItem(foodItem);
+							}
+							if (readValues.get(stringcount).length == 6){
+								
+								Item foodItem = new Item(readValues.get(stringcount)[0],Double.parseDouble(readValues.get(stringcount)[1]),Double.parseDouble(readValues.get(stringcount)[2])
+										,Integer.parseInt(readValues.get(stringcount)[3]),Integer.parseInt(readValues.get(stringcount)[4]),
+										Double.parseDouble(readValues.get(stringcount)[5]));
+								
+							Object[] tempData = { foodItem.GetName(), foodItem.getQuantity(), foodItem.GetManufacturePrice(), 
+									foodItem.GetSellPrice(), foodItem.GetReorderAmount(), foodItem.GetReorderPoint(), foodItem.GetTemperature() };
+							tableSettings.addRow(tempData);
+							
+							// ---- TEMP STORE STUFF ----
+							storeInventory.addItem(foodItem);
+							}
+						}
+						
+					
+						
+						
+						// Finally Configure The Table // 
+						inventoryTable.disable();
+						menuBar.add(inventoryTable.getTableHeader(), BorderLayout.SOUTH);
+						mainFrame.add(inventoryTable, BorderLayout.CENTER);		
+						JOptionPane.showMessageDialog(csvChooserFrame, "Loaded Item Properties");
+						mainFrame.pack();
+						
+								
+					}
+					else {
+						JOptionPane.showMessageDialog(csvChooserFrame, "Error: Input was not a CSV File");
+					}
+					
+				}
+				// Update store inventory with new one.
+				supermarket.UpdateInventory(storeInventory);
+				// Display pop-up message after completing task.
+				
+				importSaleLog.setEnabled(true);
+				importManifest.setEnabled(true);
+				exportManifest.setEnabled(true);
+			}
+			
+		});
+		
 		
 		
 		
